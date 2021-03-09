f0aba8877ac429e2507c5aae25626a85a27cb977
Rowinson Gallego <rwn.gallego@gmail.com>
2021-03-09 20:26:56 +0100

    Adding Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,17 @@ module.exports = {
                 path: `${__dirname}/static/images`,
             },
         },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "UA-191771985-1", // Google Analytics / GA
+                    // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+                    // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
+            },
+        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
